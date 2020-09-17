5d9add1e2bd6ba64cf8e64d3d3543d6e247f8a28
Williams Santos <willysantos_lai@gmail.com>
2020-09-16 21:46:44 -0600

    Version 1.0.8 update

--- mian.js
+++ mian.js
@@ -7,7 +7,6 @@ function createWindow () {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        show:false,
         webPreferences: {
             nodeIntegration: true,
         },
@@ -27,7 +26,6 @@ function createWindow () {
         log.info('esta buscando actualizaciones');
         log.warn('Some problem appears');
         autoUpdater.checkForUpdatesAndNotify();
-        mainWindow.show()
     });
 }
 
